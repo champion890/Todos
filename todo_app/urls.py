273bbb3e21fec81fee4eae966ffa3fbb3e4aedd0
sanyam1008-ai <sanyam.ai.intern@zestgeek.com>
2025-07-23 18:08:38 +0530

    add discord

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path
-from .views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView
+from .views import (
+    TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView,
+    register, discord_login, discord_callback  # 👈 Add these imports
+)
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Task Views
     path('', TaskListView.as_view(), name='task-list'),
     path('add/', TaskCreateView.as_view(), name='task-add'),
     path('edit/<int:pk>/', TaskUpdateView.as_view(), name='task-edit'),
     path('delete/<int:pk>/', TaskDeleteView.as_view(), name='task-delete'),
 
+    # Auth Views
     path('login/', auth_views.LoginView.as_view(template_name='tasks/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
+
+    #  Discord OAuth2
+    path('discord/login/', discord_login, name='discord_login'),
+    path('discord/callback/', discord_callback, name='discord_callback'),
 ]
--- todo_project/settings.py
+++ todo_project/settings.py
@@ -60,3 +60,9 @@ STATIC_URL = '/static/'
 LOGIN_URL = '/login/'  # if we not login we will be redirected to this url
 LOGIN_REDIRECT_URL = '/'      # if we login we will be redirected to this url
 LOGOUT_REDIRECT_URL = '/login/'      # if we logout we will be redirected to this url
+
+# Discord OAuth2
+DISCORD_CLIENT_ID = "1397477069398474763"
+DISCORD_CLIENT_SECRET = "9j2vQJ1li7-ZZdrC7LpgyrvlSXgVILeu"
+DISCORD_REDIRECT_URI = "http://localhost:8000/discord/callback/"
+DISCORD_API_BASE_URL = "https://discord.com/api"
